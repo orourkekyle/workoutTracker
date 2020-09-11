b3df6f53272677f6c92f3668290b0ce5c92c4f5e
zerobit <orourke_kyle@icloud.com>
2020-09-10 20:27:31 -0400

    created an index model to export schema because seeds were not populating without it (even if pathed directly to schema file), created one post route in api-routes

--- models/workouts.js
+++ models/workouts.js
@@ -1,38 +1,66 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
+
 const workoutSchema = new Schema({
-     Date: {
-        type: Date,
-        default: Date.now
+
+  Date: {
+    type: Date,
+    default: Date.now
+  },
+
+  exercises: [{
+    type: {
+      type: String,
+      unique: true,
+      required: "Enter exercise type"
     },
-  type: {
-    type: String,
-    unique: true,
-    required: "Enter exercise type"
-  },
-  name: {
-    type: Schema.Types.ObjectId,
-    ref: "Note",
-    required: "Enter Workout Name"
-  },
-  duration: {
-    type: Number,
-    required: "Please enter duration of workout"
-  },
-  weight: {
-    type: Number
-  },
-  reps: {
-    type: Number
-  },
-  sets: {
-    type: Number
-  },
-  distance: {
-    type: Number
-  } 
+
+    name: {
+
+      type: Schema.Types.ObjectId,
+      ref: "Note",
+      required: "Enter Workout Name"
+    },
+
+    duration: {
+      type: Number,
+      required: "Please enter duration of workout"
+    },
+
+    weight: {
+      type: Number
+    },
+
+    reps: {
+      type: Number
+    },
+
+    sets: {
+      type: Number
+    },
+
+    distance: {
+      type: Number
+    }
+
+  }],
+},
+
+  {
+    toJSON:
+    {
+      virtuals: true
+    }
+  }
+);
+
+
+workoutSchema.virtual("totalDuration").get(function(){
+  return this.exercises.reduce((total, exercises) => {
+    return total + exercises.duration;
+  }, 0);
 });
+const Workout = mongoose.model("workouts", workoutSchema);
 
-
-const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,19 @@
-const db = require("../models");
+const Workout = require("../models");
+const router = require("express").Router();
 
-module.exports = function(app) {
-    
-}
+// create workout route
+router.post("api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+// add exercise route
+
+// get last workout route
+
+// get workouts in range (i think "in range" means for that day)
